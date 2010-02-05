f0d3f65069b442d59266d7c0374dc1432d049a5b
Valencia <sevaor@gmail.com>
2010-02-05 00:14:59 -0700

    Annotated models

---
+++ .spec
@@ -0,0 +1,1 @@
+/var/lib/gems/1.9.1/bin/spec
--- app/models/level.rb
+++ app/models/level.rb
@@ -1,2 +1,18 @@
+# == Schema Information
+# Schema version: 20100205060128
+#
+# Table name: levels
+#
+#  id             :integer(4)      not null, primary key
+#  name           :string(100)     not null
+#  new_games      :integer(4)      not null
+#  modifications  :integer(4)      not null
+#  delete_games   :boolean(1)      not null
+#  add_categories :boolean(1)      not null
+#  manage_users   :boolean(1)      not null
+#  created_at     :datetime
+#  updated_at     :datetime
+#
+
 class Level < ActiveRecord::Base
 end
