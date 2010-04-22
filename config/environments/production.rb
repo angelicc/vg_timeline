f1af5e1e5e76def5b687419d849e2d2fc0c635bd
Diego Valencia <valencia@valencia-pcup.(none)>
2010-04-21 23:03:19 -0600

    Configured production environment 2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -6,7 +6,8 @@ VgTimeline::Application.configure do
   config.cache_classes = true
 
   # Full error reports are disabled and caching is turned on
-  config.consider_all_requests_local       = false
+  config.consider_all_requests_local       = true
+  config.action_view.debug_rjs             = true
   config.action_controller.perform_caching = true
 
   # See everything in the log (default is :info)
@@ -30,4 +31,7 @@ VgTimeline::Application.configure do
 
   # Enable threaded mode
   # config.threadsafe!
+  config.action_mailer.raise_delivery_errors = true
+  config.secret_token = 'edf9587f45b37ad3748ddcf56145c741c8d7bfb268337829458937aa026a388f22d812104b4941a18539982ae3ef48a081fdcba6b60a2649f4414f44bf26a7ca'
+  config.action_mailer.default_url_options = { :host => 'http://vg-timeline.heroku.com/' }
 end
