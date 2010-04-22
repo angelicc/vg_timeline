73862dc7c67a689d3a3dcc9a3f90e9e08da106d7
Diego Valencia <valencia@valencia-pcup.(none)>
2010-04-21 21:46:23 -0600

    Second push to Heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,7 +20,7 @@ VgTimeline::Application.configure do
 
   # Disable Rails's static asset server
   # In production, Apache or nginx will already do this
-  config.serve_static_assets = false
+  config.serve_static_assets = true
 
   # Enable serving of images, stylesheets, and javascripts from an asset server
   # config.action_controller.asset_host = "http://assets.example.com"
@@ -31,3 +31,4 @@ VgTimeline::Application.configure do
   # Enable threaded mode
   # config.threadsafe!
 end
+
